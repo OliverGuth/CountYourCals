0f452033fce39a2338dc2b673729c78cb8c302d8
Niko <blueman@frankenmail.de>
2017-01-22 13:46:09 +0100

    First DataStorage_update1

--- app/src/main/java/fhdw/bg/bfwi314b/countyourcals/fhdw/bg/bfwi314b/countyourcals/datastorage/Reader.java
+++ app/src/main/java/fhdw/bg/bfwi314b/countyourcals/fhdw/bg/bfwi314b/countyourcals/datastorage/Reader.java
@@ -1,8 +1,25 @@
 package fhdw.bg.bfwi314b.countyourcals.fhdw.bg.bfwi314b.countyourcals.datastorage;
 
 /**
- * Created by Niko on 22.01.2017.
+ * Created by Niko
+ * Read Data from Files
  */
 
 public class Reader {
+
+    public String ReadLine(String directory, String filename, int lineNumber) {
+
+        return null;
+    }
+
+    public String[] ReadFile(String directory, String filename) {
+
+        return null;
+    }
+
+    public int getLineNumber(String directory, String filename, String lineContent) {
+
+        return -1;
+    }
+
 }
